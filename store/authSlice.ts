186e140173765d66d686171bcb66c414b2f4158d
Induwara <induwara2k@gmail.com>
2025-02-23 11:42:37 +0530

    feat: implement supabase auth

---
+++ store/authSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface AuthState {
+  user: any | null;
+}
+
+const initialState: AuthState = {
+  user: null,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<any | null>) => {
+      state.user = action.payload;
+    },
+    logoutUser: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { setUser, logoutUser } = authSlice.actions;
+export default authSlice.reducer;
